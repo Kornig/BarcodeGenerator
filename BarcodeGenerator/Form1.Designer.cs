cc5e6e42cb3937953037809b1ff57baf0d73e597
Marcin <marcins137wp.pl>
2023-07-23 23:18:49 +0200

    Add ZXing.Net and ZXing.Net.Bindings.Windows.Compatibility NuGet packages. Create simple app to generate barcode bitmap images for random strings.

--- BarcodeGenerator/Form1.Designer.cs
+++ BarcodeGenerator/Form1.Designer.cs
@@ -28,25 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
             dataGridView1 = new DataGridView();
+            GenerateBtn = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // dataGridView1
             // 
             dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(16, 14);
+            dataGridView1.Location = new Point(12, 59);
             dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowTemplate.Height = 25;
-            dataGridView1.Size = new Size(761, 334);
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = SystemColors.Control;
+            dataGridViewCellStyle1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridView1.RowTemplate.Height = 50;
+            dataGridView1.RowTemplate.Resizable = DataGridViewTriState.True;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.Size = new Size(662, 625);
             dataGridView1.TabIndex = 0;
+            // 
+            // GenerateBtn
+            // 
+            GenerateBtn.Location = new Point(12, 22);
+            GenerateBtn.Name = "GenerateBtn";
+            GenerateBtn.Size = new Size(662, 31);
+            GenerateBtn.TabIndex = 1;
+            GenerateBtn.Text = "Generate";
+            GenerateBtn.UseVisualStyleBackColor = true;
+            GenerateBtn.Click += GenerateBtn_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 659);
+            ClientSize = new Size(686, 698);
+            Controls.Add(GenerateBtn);
             Controls.Add(dataGridView1);
             Name = "Form1";
             Text = "Form1";
@@ -57,5 +81,6 @@
         #endregion
 
         private DataGridView dataGridView1;
+        private Button GenerateBtn;
     }
 }
--- BarcodeGenerator/Form1.cs
+++ BarcodeGenerator/Form1.cs
@@ -1,10 +1,49 @@
+
+
+using System.Diagnostics;
+using System.Text;
+using ZXing;
+
 namespace BarcodeGenerator
 {
     public partial class Form1 : Form
     {
+        private const byte _numOfStr = 20;
+        private const byte _strLen = 16;
+        private const string _charRange = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void GenerateBtn_Click(object sender, EventArgs e)
+        {
+            var strings = new List<string>(_numOfStr);
+            Random rnd = new();
+            StringBuilder tempSB = new();
+            BarcodeWriterGeneric bw = new()
+            {
+                Format = BarcodeFormat.CODE_128
+            };
+
+            for (int i = 0; i < _numOfStr; ++i)
+            {
+                for (int j = 0; j < _strLen; ++j)
+                {
+                    tempSB.Append(_charRange[rnd.Next(_charRange.Length)]);
+                }
+                strings.Add(tempSB.ToString());
+                tempSB.Clear();
+            }
+
+            //generating new list of objects containing anonymous type objects with 2 properties: string value and barcode image
+            dataGridView1.DataSource = strings.Select(s => new { Value = s, Barcode = bw.WriteAsBitmap(s) }).ToList();
+
+            //Debug - checking types of barcode image
+            Debug.WriteLine(bw.WriteAsBitmap("123").GetType().ToString());
+            Debug.WriteLine(dataGridView1.Columns[1].CellType.ToString());
+
         }
     }
 }
